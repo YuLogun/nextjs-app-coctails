f0082bb0afad43c3b9c09bf78c7081d7fd1678c9
Iuliia <54540652+YuLogun@users.noreply.github.com>
2020-06-26 22:39:27 +0400

    adde _app.js, started building th logic

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../../styles/header.module.css";
+
+const Header = () => (
+  <div className={styles.container}>
+    <h1>Cocktails - love</h1>
+  </div>
+);
+
+export default Header;
